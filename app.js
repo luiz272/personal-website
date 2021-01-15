b237425d6d917bb568e7cce153a629ecca33ba4a
Luiz Felipe Lima <luizf.lima@pm.me>
2021-01-15 00:08:48 -0300

    General CV Updates and Database integration start

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/personal-site-DB', {useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/personalSiteDB', {useNewUrlParser:true, useUnifiedTopology: true});
 
 app.listen(3000, function(){
   console.log('Server running on port 3000');
@@ -25,7 +25,35 @@ app.listen(3000, function(){
 
 app.get('/', function(req, res) {
 
-  res.render('main', {page:'main'});
+  const mainPageSchema = {
+      page:String,
+      language:String,
+      intro: {
+        main: String,
+        sub: String,
+        btn: String
+      }
+    };
+
+  const MainPageModel = mongoose.model('main',mainPageSchema,'main');
+
+  MainPageModel.find({}, function(err, results) {
+
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('main',
+        {
+          page: results[0].page,
+          language: results[0].language,
+          introTitle: results[0].intro.main,
+          introSubtitle: results[0].intro.sub,
+          introButton: results[0].intro.btn
+        }
+      );
+    }
+
+  });
 
 });
 
