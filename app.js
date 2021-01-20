7c548c9f2dca58ad40ac2acc62ca02db450213ce
Luiz Felipe Lima <luizf.lima@pm.me>
2021-01-19 23:24:48 -0300

    Updated CV page to have multilanguage support, updated navigation for dynamic changing of links and languages, small bug fixes

--- app.js
+++ app.js
@@ -26,6 +26,19 @@ app.listen(3000, function(){
 
 // Schemas and Models //
 
+const headerSchema = {
+  language:String,
+  pageTitle:String,
+  navbar:{
+    home:String,
+    about:String,
+    projects:String,
+    contact:String
+  }
+}
+
+const HeaderModel = mongoose.model('header', headerSchema, 'header');
+
 const mainPageSchema = {
     page:String,
     language:String,
@@ -89,6 +102,128 @@ const mainPageSchema = {
 
 const MainPageModel = mongoose.model('main',mainPageSchema,'main');
 
+const cvSchema = {
+
+  page:String,
+  language:String,
+  name:String,
+  intro:{
+    age:String,
+    nationality:String,
+    location:String,
+    homecity:String
+  },
+  education:{
+    title:String,
+    bachelors:String
+  },
+  work:{
+    ms:{
+      title:String,
+      description:String,
+      project4:{
+        title:String,
+        period:String,
+        role:String,
+        technologies:String,
+        descTitle:String,
+        descBody:String
+      },
+      project3:{
+        title:String,
+        period:String,
+        role:String,
+        technologies:String,
+        descTitle:String,
+        descBody:String
+      },
+      project2:{
+        title:String,
+        period:String,
+        role:String,
+        technologies:String,
+        descTitle:String,
+        descBody:String
+      },
+      project1:{
+        title:String,
+        period:String,
+        role:String,
+        technologies:String,
+        descTitle:String,
+        descBody:String
+      }
+    },
+    xp:{
+      title:String,
+      item1:String,
+      item2:String,
+      item3:String
+    },
+    b3:{
+      title:String,
+      project:String,
+      objective:String
+    }
+  },
+  academicExperience:{
+    title:String,
+    si:{
+      title:String,
+      project:String,
+      activities:String
+    },
+    ta:{
+      title:String,
+      activities:String
+    }
+  },
+  technologies:{
+    dataScience:{
+      title:String,
+      item1:String,
+      item2:String
+    },
+    webDev:{
+      title:String
+    },
+    db:{
+      title:String
+    },
+    misc:{
+      title:String,
+      item1:String,
+      item2:String,
+      item3:String,
+      item4:String
+    }
+  },
+  languages:{
+    title:String,
+    pt:String,
+    en:String,
+    hu:String,
+    es:String
+  },
+  awards:{
+    title:String,
+    valuation:String,
+    monography:String,
+    macroeconomics:String,
+    microeconomics:String
+  },
+  courses:{
+    title:String,
+    insurance:String,
+    odsc:String,
+    pushkin:String,
+    impacta:String
+  },
+  disclaimer:String
+}
+
+const CVModel = mongoose.model('cv', cvSchema, 'cv');
+
 // Features //
 // Main Get - Root //
 
@@ -107,11 +242,18 @@ app.get('/:lang', function(req, res) {
     if (err) {
       console.log(err);
     } else {
-      res.render('main',
-        {
-          mainSource: results[0]
+
+      HeaderModel.find({language:req.params.lang.toUpperCase()}, function(hErr, hResults) {
+
+        if (hErr) {
+          console.log(hErr);
+        } else {
+          res.render('main',{mainSource: results[0], headerSource:hResults[0]});
         }
-      );
+
+      });
+
+
     }
 
   });
@@ -122,6 +264,24 @@ app.get('/:lang', function(req, res) {
 
 app.get('/:lang/cv', function(req, res) {
 
-  res.render('cv', {page:'cv'});
+  CVModel.find({language:req.params.lang.toUpperCase()}, function(err, results) {
+
+    if (err) {
+      console.log(err);
+    } else {
+
+      HeaderModel.find({language:req.params.lang.toUpperCase()}, function(hErr, hResults) {
+
+        if (hErr) {
+          console.log(hErr);
+        } else {
+          res.render('cv', {mainSource:results[0], headerSource:hResults[0]});
+        }
+
+      });
+
+    }
+
+  });
 
 });
