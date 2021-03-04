2d88668cd0abd71dc7343827d5fc7bf9b23342f3
Luiz Felipe Lima <luizf.lima@pm.me>
2021-03-03 23:01:39 -0300

    Add cloud connection and minor corrections

--- app.js
+++ app.js
@@ -11,234 +11,234 @@ const ejs = require('ejs');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 app.use('/en', express.static(__dirname + '/public'));
 app.use('/pt', express.static(__dirname + '/public'));
 app.use('/hu', express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost:27017/personalSiteDB', {useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://<user>:<password>@<server>/<DB>?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function () {
   console.log('Server running on port 3000');
 });
 
 // Schemas and Models //
 
 const headerSchema = {
-  language:String,
-  pageTitle:String,
-  navbar:{
-    home:String,
-    about:String,
-    projects:String,
-    contact:String
+  language: String,
+  pageTitle: String,
+  navbar: {
+    home: String,
+    about: String,
+    projects: String,
+    contact: String
   }
 }
 
 const HeaderModel = mongoose.model('header', headerSchema, 'header');
 
 const mainPageSchema = {
-    page:String,
-    language:String,
-    intro: {
-      main: String,
-      sub: String,
+  page: String,
+  language: String,
+  intro: {
+    main: String,
+    sub: String,
+    btn: String
+  },
+  about: {
+    title: String,
+    item1: {
+      title: String,
+      text: String,
       btn: String
     },
-    about: {
+    item2: {
       title: String,
-      item1: {
-        title: String,
-        text: String,
-        btn: String
-      },
-      item2: {
-        title: String,
-        list: {
-          item1: String,
-          item2: String,
-          item3: String,
-          item4: String,
-          item5: String,
-        }
-      },
-      item3: {
-        title: String,
-        list: {
-          item1: String,
-          item2: String,
-          item3: String,
-          item4: String,
-          item5: String,
-        }
+      list: {
+        item1: String,
+        item2: String,
+        item3: String,
+        item4: String,
+        item5: String,
       }
     },
-    projects: {
+    item3: {
       title: String,
-      item1: {
-        title: String,
-        description: String
-      },
-      item2: {
-        title: String,
-        description: String
-      },
-      item3: {
-        title: String,
-        description: String
-      },
-      btn: String
-    },
-    contact: {
-      title: String,
-      name: String,
-      email: String,
-      message: String,
-      btn: String
+      list: {
+        item1: String,
+        item2: String,
+        item3: String,
+        item4: String,
+        item5: String,
+      }
     }
-  };
+  },
+  projects: {
+    title: String,
+    item1: {
+      title: String,
+      description: String
+    },
+    item2: {
+      title: String,
+      description: String
+    },
+    item3: {
+      title: String,
+      description: String
+    },
+    btn: String
+  },
+  contact: {
+    title: String,
+    name: String,
+    email: String,
+    message: String,
+    btn: String
+  }
+};
 
-const MainPageModel = mongoose.model('main',mainPageSchema,'main');
+const MainPageModel = mongoose.model('main', mainPageSchema, 'main');
 
 const cvSchema = {
 
-  page:String,
-  language:String,
-  name:String,
-  intro:{
-    age:String,
-    nationality:String,
-    location:String,
-    homecity:String
+  page: String,
+  language: String,
+  name: String,
+  intro: {
+    age: String,
+    nationality: String,
+    location: String,
+    homecity: String
   },
-  education:{
-    title:String,
-    bachelors:String
+  education: {
+    title: String,
+    bachelors: String
   },
-  work:{
-    title:String,
-    ms:{
-      title:String,
-      description:String,
-      project4:{
-        title:String,
-        period:String,
-        role:String,
-        technologies:String,
-        descTitle:String,
-        descBody:String
+  work: {
+    title: String,
+    ms: {
+      title: String,
+      description: String,
+      project4: {
+        title: String,
+        period: String,
+        role: String,
+        technologies: String,
+        descTitle: String,
+        descBody: String
       },
-      project3:{
-        title:String,
-        period:String,
-        role:String,
-        technologies:String,
-        descTitle:String,
-        descBody:String
+      project3: {
+        title: String,
+        period: String,
+        role: String,
+        technologies: String,
+        descTitle: String,
+        descBody: String
       },
-      project2:{
-        title:String,
-        period:String,
-        role:String,
-        technologies:String,
-        descTitle:String,
-        descBody:String
+      project2: {
+        title: String,
+        period: String,
+        role: String,
+        technologies: String,
+        descTitle: String,
+        descBody: String
       },
-      project1:{
-        title:String,
-        period:String,
-        role:String,
-        technologies:String,
-        descTitle:String,
-        descBody:String
+      project1: {
+        title: String,
+        period: String,
+        role: String,
+        technologies: String,
+        descTitle: String,
+        descBody: String
       }
     },
-    xp:{
-      title:String,
-      operational:{
-        title:String,
-        item1:String,
-        item2:String,
-        item3:String
+    xp: {
+      title: String,
+      operational: {
+        title: String,
+        item1: String,
+        item2: String,
+        item3: String
       },
-      products:{
-        title:String,
-        item1:String,
+      products: {
+        title: String,
+        item1: String,
       },
-      commercial:{
-        title:String,
-        item1:String,
-        item2:String
+      commercial: {
+        title: String,
+        item1: String,
+        item2: String
       }
     },
-    b3:{
-      title:String,
-      project:String,
-      objective:String
+    b3: {
+      title: String,
+      project: String,
+      objective: String
     }
   },
-  academicExperience:{
-    title:String,
-    si:{
-      title:String,
-      project:String,
-      activities:{
-        title:String,
-        item1:String,
-        item2:String,
-        item3:String
+  academicExperience: {
+    title: String,
+    si: {
+      title: String,
+      project: String,
+      activities: {
+        title: String,
+        item1: String,
+        item2: String,
+        item3: String
       }
     },
-    ta:{
-      title:String,
-      activities:String
+    ta: {
+      title: String,
+      activities: String
     }
   },
-  technologies:{
-    title:String,
-    dataScience:{
-      title:String,
-      item1:String,
-      item2:String
+  technologies: {
+    title: String,
+    dataScience: {
+      title: String,
+      item1: String,
+      item2: String
     },
-    webDev:{
-      title:String
+    webDev: {
+      title: String
     },
-    db:{
-      title:String
+    db: {
+      title: String
     },
-    misc:{
-      title:String,
-      item1:String,
-      item2:String,
-      item3:String,
-      item4:String
+    misc: {
+      title: String,
+      item1: String,
+      item2: String,
+      item3: String,
+      item4: String
     }
   },
-  languages:{
-    title:String,
-    pt:String,
-    en:String,
-    hu:String,
-    es:String
+  languages: {
+    title: String,
+    pt: String,
+    en: String,
+    hu: String,
+    es: String
   },
-  awards:{
-    title:String,
-    valuation:String,
-    monography:String,
-    macroeconomics:String,
-    microeconomics:String
+  awards: {
+    title: String,
+    valuation: String,
+    monography: String,
+    macroeconomics: String,
+    microeconomics: String
   },
-  courses:{
-    title:String,
-    insurance:String,
-    odsc:String,
-    pushkin:String,
-    impacta:String
+  courses: {
+    title: String,
+    insurance: String,
+    odsc: String,
+    pushkin: String,
+    impacta: String
   },
-  disclaimer:String
+  disclaimer: String
 }
 
 const CVModel = mongoose.model('cv', cvSchema, 'cv');
@@ -246,7 +246,7 @@ const CVModel = mongoose.model('cv', cvSchema, 'cv');
 // Features //
 // Main Get - Root //
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 
   res.redirect('/en');
 
@@ -254,20 +254,20 @@ app.get('/', function(req, res) {
 
 // Main Get - Language Specific //
 
-app.get('/:lang', function(req, res) {
+app.get('/:lang', function (req, res) {
 
-  MainPageModel.find({language:req.params.lang.toUpperCase()}, function(err, results) {
+  MainPageModel.find({ language: req.params.lang.toUpperCase() }, function (err, results) {
 
     if (err) {
       console.log(err);
     } else {
 
-      HeaderModel.find({language:req.params.lang.toUpperCase()}, function(hErr, hResults) {
+      HeaderModel.find({ language: req.params.lang.toUpperCase() }, function (hErr, hResults) {
 
         if (hErr) {
           console.log(hErr);
         } else {
-          res.render('main',{mainSource: results[0], headerSource:hResults[0]});
+          res.render('main', { mainSource: results[0], headerSource: hResults[0] });
         }
 
       });
@@ -281,20 +281,20 @@ app.get('/:lang', function(req, res) {
 
 // CV Get //
 
-app.get('/:lang/cv', function(req, res) {
+app.get('/:lang/cv', function (req, res) {
 
-  CVModel.find({language:req.params.lang.toUpperCase()}, function(err, results) {
+  CVModel.find({ language: req.params.lang.toUpperCase() }, function (err, results) {
 
     if (err) {
       console.log(err);
     } else {
 
-      HeaderModel.find({language:req.params.lang.toUpperCase()}, function(hErr, hResults) {
+      HeaderModel.find({ language: req.params.lang.toUpperCase() }, function (hErr, hResults) {
 
         if (hErr) {
           console.log(hErr);
         } else {
-          res.render('cv', {mainSource:results[0], headerSource:hResults[0], lock:'true'});
+          res.render('cv', { mainSource: results[0], headerSource: hResults[0], lock: 'true' });
         }
 
       });
@@ -302,34 +302,5 @@ app.get('/:lang/cv', function(req, res) {
     }
 
   });
-
-});
-
-
-// CV with Certificate Get //
-
-app.get('/:lang/cv/unlock', function(req, res) {
-
-  app.use('/' + req.params.lang.toLowerCase() + '/cv/unlock', express.static(__dirname + '/public'));
-
-  CVModel.find({language:req.params.lang.toUpperCase()}, function(err, results) {
-
-    if(err) {
-      console.log(err);
-    } else {
-
-      HeaderModel.find({language:req.params.lang.toUpperCase()}, function(hErr, hResults) {
-
-          if(hErr) {
-            console.log(hErr);
-          } else {
-            res.render('cv',{mainSource:results[0], headerSource:hResults[0], lock:'false'})
-          }
-
-      })
-
-    }
-
-  })
 
 });
